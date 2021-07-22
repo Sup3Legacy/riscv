af8d835ed393fee8bdef4f5e2083b626fe784ae3
Constantin Gierczak--Galle <constantin.gierczak-galle@ens.fr>
2021-07-22 10:36:01 +0200

    Add working sentropy logic

--- src/register/sentropy.rs
+++ src/register/sentropy.rs
@@ -1,5 +1,7 @@
-use bit_field::BitField;
+use num_derive::FromPrimitive;
+use num_traits::FromPrimitive;
 
+#[derive(FromPrimitive)]
 pub enum EntropyStatus {
     BIST = 0,
     ES16 = 1,
@@ -22,11 +24,11 @@ unsafe fn read_raw() -> usize {
 
 pub fn read() -> Option<Entropy> {
     let raw_data = unsafe { read_raw() };
-    match (raw_data >> 30) & 0b11 {
+    match FromPrimitive::from_usize(((raw_data >> 30) & 0b11) as u32) {
         EntropyStatus::BIST => Some(Entropy::Bist),
         EntropyStatus::ES16 => Some(Entropy::Es16(raw_data as u16)),
         EntropyStatus::WAIT => Some(Entropy::Wait),
         EntropyStatus::DEAD => Some(Entropy::Dead),
-        _ => None
+        _ => None,
     }
 }
